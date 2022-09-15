002d3725b9cd6a031241b820c0cc66b813ae5064
sayazean <105099566+sayazean@users.noreply.github.com>
2022-09-16 02:58:44 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -182,8 +182,8 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**ZELDA USERBOT ERROR REPORT**\n\n"
-                    link = "[Channel Support](https://t.me/zldprojects)"
+                    text = "**Z USERBOT ERROR REPORT**\n\n"
+                    link = "[Channel Support](https://t.me/ZeanSupport)"
                     text += "Jika mau, Anda bisa melaporkan error ini, "
                     text += f"Cukup forward saja pesan ini ke {link}.\n\n"
 
@@ -192,7 +192,7 @@ def register(**args):
                     ftext += "\nkami hanya mencatat fakta error dan tanggal,"
                     ftext += "\nkami menghormati privasi Anda."
                     ftext += "\nJika mau, Anda bisa melaporkan error ini,"
-                    ftext += "\ncukup forward saja pesan ini ke @UnrealZelda"
+                    ftext += "\ncukup forward saja pesan ini ke @zeannihboss"
                     ftext += "\n================================\n\n"
                     ftext += "--------BEGIN USERBOT TRACEBACK LOG--------\n"
                     ftext += "\nTanggal : " + date
